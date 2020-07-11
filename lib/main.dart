d4b4f4664f3e7ced8cf3df2a2f4ff2ac49157263
senaybee <senay_berihu@outlook.com>
2020-07-11 14:39:13 -0700

    Add trailing favorite icon and alreadySaved flag

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,18 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  final Set<WordPair> _saved = {};
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
-        title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ));
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null),
+    );
   }
 
   Widget _buildSuggestions() {
